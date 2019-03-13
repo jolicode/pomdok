f980d3bafec659b00b6339125b1480b5c4f9907e
Baptiste Leduc <baptiste.leduc@gmail.com>
2019-03-13 23:22:53 +0100

    complete check command 👌

--- checkCommand.go
+++ checkCommand.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"os/exec"
 	"runtime"
+	"strings"
 
+	"github.com/fatih/color"
 	"github.com/mkideal/cli"
 	"github.com/olekukonko/tablewriter"
 )
@@ -17,11 +21,12 @@ var checkCommand = &cli.Command{
 	Desc: "this is a check command",
 	Argv: func() interface{} { return new(start) },
 	Fn: func(ctx *cli.Context) error {
+		title := color.New(color.Bold).SprintFunc()
 		data := [][]string{
-			[]string{"System", runtime.GOOS, "Operating System you run"},
-			[]string{"B", "The Very very Bad Man", "288"},
-			[]string{"C", "The Ugly", "120"},
-			[]string{"D", "The Gopher", "800"},
+			[]string{title("System"), sprintCheckSystem(), "Operating System you run"},
+			[]string{title("PHP"), sprintCheckCliExists("php"), "PHP runtime"},
+			[]string{title("nginx"), sprintCheckCliExists("nginx"), "Proxy server"},
+			[]string{title("Symfony"), sprintCheckCliExists("symfony"), "Symfony CLI"},
 		}
 
 		table := tablewriter.NewWriter(os.Stdout)
@@ -34,4 +39,34 @@ var checkCommand = &cli.Command{
 
 		return nil
 	},
+}
+
+func sprintCheckSystem() string {
+	system := runtime.GOOS
+	if system == "linux" || system == "darwin" {
+		return color.GreenString(system)
+	} else {
+		return color.RedString(system)
+	}
+}
+
+func sprintCheckCliExists(command string) string {
+	out := checkCliExists(command)
+	if out == "" {
+		out = color.RedString("Not-found")
+	} else {
+		out = color.GreenString(out)
+	}
+
+	return out
+}
+
+func checkCliExists(command string) string {
+	return execCommand(fmt.Sprintf("which %s", command))
+}
+
+func execCommand(command string) string {
+	out, _ := exec.Command("sh", "-c", command).Output()
+
+	return strings.TrimSuffix(string(out), "\n")
 }
