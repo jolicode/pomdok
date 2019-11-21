455780ddd243777a0fbc348c242fb241adab940d
goreleaserbot <goreleaser@carlosbecker.com>
2019-11-21 14:22:21 +0100

    Brew formula update for pomdok version v1.2.0

--- Formula/pomdok.rb
+++ Formula/pomdok.rb
@@ -2,9 +2,18 @@
 class Pomdok < Formula
   desc "Simple wrapper to Symfony Go Binary for multi-app."
   homepage "https://github.com/jolicode/pomdok"
-  url "https://github.com/jolicode/pomdok/releases/download/v1.1.0/pomdok_1.1.0_Darwin_x86_64.tar.gz"
-  version "1.1.0"
-  sha256 "df02e9977f9991deedd48a9e5b8e19d9d03a1ea999d705941071b4a5df28c2cd"
+  version "1.2.0"
+  bottle :unneeded
+
+  if OS.mac?
+    url "https://github.com/jolicode/pomdok/releases/download/v1.2.0/pomdok_1.2.0_Darwin_x86_64.tar.gz"
+    sha256 "f41c4b3bb95cb8f158d7cac4acbfafb03bd145662373a741a18371e32aae8f2d"
+  elsif OS.linux?
+    if Hardware::CPU.intel?
+      url "https://github.com/jolicode/pomdok/releases/download/v1.2.0/pomdok_1.2.0_Linux_x86_64.tar.gz"
+      sha256 "214504f533471900fe019ed8afd925a9ee915ca90e55a8e7df448231af672d70"
+    end
+  end
   
   depends_on "wget"
   depends_on "nss"
